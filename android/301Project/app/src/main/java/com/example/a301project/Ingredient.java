ce40c437a5f1d945e52d2aee4fb0b55ae179fdac
yuihan <juyi@ualberta.ca>
2022-10-22 22:09:49 -0600

    add ingredient class

---
+++ android/301Project/app/src/main/java/com/example/a301project/Ingredient.java
@@ -0,0 +1,12 @@
+package com.example.a301project;
+
+public class Ingredient {
+    private String name;
+    private String location;
+    private String bbd;
+    private String category;
+    private String unit;
+    private int amount;
+
+
+}
