5ed02d4f24dffff5135190bc2495ce76ebd79dff
yuihan <juyi@ualberta.ca>
2022-10-25 15:20:08 -0600

    added customList class added ingredient listview layout (including ingredient_content.xml) added IngredientController but no implementation

--- android/301Project/app/src/main/java/com/example/a301project/IngredientActivity.java
+++ android/301Project/app/src/main/java/com/example/a301project/IngredientActivity.java
@@ -2,8 +2,19 @@ package com.example.a301project;
 
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class IngredientActivity extends NavActivity {
+    ListView ingredientList;
+    ArrayAdapter<Ingredient> ingredientAdapter;
+    ArrayList<Ingredient> dataList;
+    public int position = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +22,28 @@ public class IngredientActivity extends NavActivity {
         // We have to put our layout in the space for the content
         ViewGroup content = findViewById(R.id.nav_content);
         getLayoutInflater().inflate(R.layout.activity_ingredient, content, true);
+        FirebaseFirestore db;
+
+        // create list of ingredients
+        ingredientList = findViewById(R.id.IngredientList);
+        //initialize attributes as empty
+        String []ingredients ={"pizza"};
+        String []locations = {"bed"};
+        String []bbds = {"2022-12-01"};
+        Integer []amounts = {1};
+        String []units = {"slice"};
+        String []categories = {"lunch"};
+
+        dataList = new ArrayList<>();
+
+        //initialize dataList
+        for (int i=0; i<ingredients.length;i++) {
+            dataList.add(new Ingredient(ingredients[i],locations[i],bbds[i],amounts[i],units[i],categories[i]));
+        }
+        ingredientAdapter = new CustomList(this,dataList);
+        ingredientList.setAdapter(ingredientAdapter);
+
+
 
         // Set the correct button to be selected
         bottomNav.getMenu().findItem(R.id.action_ingredients).setChecked(true);
---
+++ android/301Project/app/src/main/java/com/example/a301project/IngredientController.java
@@ -0,0 +1,6 @@
+package com.example.a301project;
+
+public class IngredientController {
+    // connect to firebase and handles add and delete
+
+}
