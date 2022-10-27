0be49d38ca814c93eaf2fc1c0e1580591319fda2
yuihan <juyi@ualberta.ca>
2022-10-27 14:40:26 -0600

    Added ingredient sorter

--- android/301Project/app/src/main/java/com/example/a301project/IngredientActivity.java
+++ android/301Project/app/src/main/java/com/example/a301project/IngredientActivity.java
@@ -1,16 +1,19 @@
 package com.example.a301project;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -29,6 +32,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -41,6 +45,9 @@ public class IngredientActivity extends NavActivity implements AddEditIngredient
     private ArrayAdapter<Ingredient> ingredientAdapter;
     private Spinner ingredientSpinner;
     private ArrayList<Ingredient> dataList;
+    private String[] sortOptions = {"Entry", "Location", "Expiry", "Category"};
+    private Spinner sortSpinner;
+    private Switch sortSwitch;
     public int position = -1;
 
     Button addButton;
@@ -56,28 +63,47 @@ public class IngredientActivity extends NavActivity implements AddEditIngredient
         addButton = findViewById(R.id.add_ingredient_button);
         // create list of ingredients
         ingredientList = findViewById(R.id.ingredientListView);
-
-//        //initialize attributes as empty
-//        String []ingredients ={"pizza"};
-//        String []locations = {"bed"};
-//        String []bbds = {"2022-12-01"};
-//        Integer []amounts = {1};
-//        String []units = {"slice"};
-//        String []categories = {"lunch"};
-
         dataList = new ArrayList<>();
 
-
-//        //initialize dataList
-//        for (int i=0; i<ingredients.length;i++) {
-//            dataList.add(new Ingredient(ingredients[i],amounts[i],bbds[i],locations[i],units[i],categories[i]));
-//            //nameUnit.put(ingredients[i],units[i]);
-//        }
-
+        // Style header text to bold
+        ViewGroup ingredientHeader = content.findViewById(R.id.ingredientHeader);
+        for (int i = 0; i < ingredientHeader.getChildCount(); i++) {
+            View child = ingredientHeader.getChildAt(i);
+            if (child instanceof TextView) {
+                ((TextView) child).setTypeface(null, Typeface.BOLD);
+            }
+        }
 
         ingredientAdapter = new CustomList(this,dataList);
         ingredientList.setAdapter(ingredientAdapter);
 
+
+        // ingredient sort by title, location, expiry date, category
+        sortSpinner = findViewById(R.id.ingredientSortSpinner);
+        sortSwitch = findViewById(R.id.ingredientSortSwitch);
+        ArrayAdapter<String> sortAdapter = new ArrayAdapter<>(this,com.google.android.material.R.layout.support_simple_spinner_dropdown_item, sortOptions);
+        sortSpinner.setAdapter(sortAdapter);
+        sortSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                sortDataBySpinner();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // nothing happens
+            }
+        });
+        sortSwitch.setChecked(true);
+        sortSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                sortDataBySpinner();
+            }
+        });
+        sortDataBySpinner();
+
+        // on item  click in ingredient list, editor appears
         ingredientList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -88,6 +114,7 @@ public class IngredientActivity extends NavActivity implements AddEditIngredient
             }
         });
 
+        // on add button click in ingredient list, adder appears
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -130,6 +157,29 @@ public class IngredientActivity extends NavActivity implements AddEditIngredient
 
         // Set the correct button to be selected
         bottomNav.getMenu().findItem(R.id.action_ingredients).setChecked(true);
+    }
+
+    public void sortDataBySpinner() {
+        sortSpinner = findViewById(R.id.ingredientSortSpinner);
+        sortSwitch = findViewById(R.id.ingredientSortSwitch);
+
+        // retrieve the sort information
+        String sortBy = sortSpinner.getSelectedItem().toString();
+        Integer asc = sortSwitch.isChecked() ? 1:-1;
+
+        Collections.sort(dataList, (Ingredient i1, Ingredient i2) -> {
+                    if (sortBy.equals(sortOptions[0])) {
+                        return asc * i1.getName().compareTo(i2.getName());
+                    } else if (sortBy.equals(sortOptions[1])) {
+                        return asc * i1.getLocation().compareTo(i2.getLocation());
+                    } else if (sortBy.equals(sortOptions[2])) {
+                        return asc * i1.getbbd().compareTo(i2.getbbd());
+                    } else {
+                        return asc * i1.getCategory().compareTo(i2.getCategory());
+                    }
+                }
+        );
+        ingredientAdapter.notifyDataSetChanged();
     }
 
     public void addIngredient(Ingredient ingredient) {
