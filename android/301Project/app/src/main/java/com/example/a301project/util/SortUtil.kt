7c5699f770e00eb7f0a56ebb4f02f41eb0c886ae
ZhiyuanYu133 <77706312+ZhiyuanYu133@users.noreply.github.com>
2022-10-28 13:26:13 -0600

    Update SortUtil.kt

--- android/301Project/app/src/main/java/com/example/a301project/util/SortUtil.kt
+++ android/301Project/app/src/main/java/com/example/a301project/util/SortUtil.kt
@@ -1,15 +1,30 @@
-﻿package com.example.a301project.util
+package com.example.a301project.util;
 
-import com.example.a301project.shopping.entity.ShoppingItem
+import com.example.a301project.shopping.entity.ShoppingItem;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 
-object SortUtil {
-    fun sortShoppingItemByIngredient(dataList:MutableList<ShoppingItem>){
-        dataList.sortBy { it.ingredient }
+public class SortUtil {
+
+
+    public static  void sortShoppingItemByIngredient(List<ShoppingItem> dataList){
+        Collections.sort(dataList, new Comparator<ShoppingItem>() {
+            @Override
+            public int compare(ShoppingItem o1, ShoppingItem o2) {
+                return o1.getIngredient().compareTo(o2.getIngredient());
+            }
+        });
     }
 
-    fun sortShoppingItemByCategory(dataList:MutableList<ShoppingItem>){
-       dataList.sortBy { it.category }
+    public static  void sortShoppingItemByCategory(List<ShoppingItem> dataList){
+        Collections.sort(dataList, new Comparator<ShoppingItem>() {
+            @Override
+            public int compare(ShoppingItem o1, ShoppingItem o2) {
+                return o1.getCategory().compareTo(o2.getCategory());
+            }
+        });
     }
-
 }
