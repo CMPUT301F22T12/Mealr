9aea1bf3fb7c5d14c2fbface6efc5ce1e1f83a2e
yuihan <juyi@ualberta.ca>
2022-11-19 19:21:34 -0700

    meal plan update

--- android/301Project/app/src/main/java/com/example/a301project/AddEditMealPlanFragment.java
+++ android/301Project/app/src/main/java/com/example/a301project/AddEditMealPlanFragment.java
@@ -1,8 +1,13 @@
 package com.example.a301project;
 
+import android.widget.Button;
+import android.widget.ListView;
+
 import androidx.fragment.app.DialogFragment;
 
 public class AddEditMealPlanFragment extends DialogFragment {
+    private ListView ingredientList;
+    private Button addIngredientButton;
     private AddEditMealPlanFragment.OnFragmentInteractionListener listener;
     public interface OnFragmentInteractionListener {
         void onConfirmPressed(MealPlan currentMealPlan, boolean createNewMeal);
